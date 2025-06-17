1a6e331a02a1f8a677122009cf53ac3764850dae
SebastianNovaOne <sebaxnovax@gmail.com>
2025-06-17 03:05:52 -0400

    Se añade constructor del PanelPrincipal, con valor 5 para que haya 5 de cada producto disponible, se añaden setbounds para probar tamaño despues.

--- src/main/java/grafica/PanelPrincipal.java
+++ src/main/java/grafica/PanelPrincipal.java
@@ -3,8 +3,26 @@ package grafica;
 import javax.swing.JPanel;
 import logica.Expendedor;
 
+import java.awt.*;
+
 public class PanelPrincipal extends JPanel{
     private PanelComprador comprador;
     private PanelExpendedor expendedor;
     private Expendedor expendedorLogica;
+
+    public PanelPrincipal() {
+        setBackground(Color.WHITE);
+        setLayout(null);
+
+        expendedorLogica = new Expendedor(5);
+        expendedor = new PanelExpendedor(expendedorLogica);
+        comprador = new PanelComprador(expendedor);
+
+        expendedor.setBounds(0, 0, 0, 0);
+        comprador.setBounds(0, 0, 0, 0);
+
+        add(expendedor);
+        add(comprador);
+        comprador.setVisible(true);
+    }
 }
