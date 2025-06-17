ef6b5f26a21163fc7c1a318957430cc8d3de4be6
SebastianNovaOne <sebaxnovax@gmail.com>
2025-06-17 02:53:35 -0400

    Se crea clase PanelPrincipal con atributos de Expendedor y de 2 clases que creare.

---
+++ src/main/java/grafica/PanelPrincipal.java
@@ -0,0 +1,10 @@
+package grafica;
+
+import javax.swing.JPanel;
+import logica.Expendedor;
+
+public class PanelPrincipal extends JPanel{
+    private PanelComprador comprador;
+    private PanelExpendedor expendedor;
+    private Expendedor expendedorLogica;
+}
