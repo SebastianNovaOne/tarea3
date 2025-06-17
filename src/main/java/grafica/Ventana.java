3c31bb6862833d29e12d33e7908f34a7442145cc
SebastianNovaOne <sebaxnovax@gmail.com>
2025-06-17 02:44:56 -0400

    Se crea clase Ventana, que se usara en la clase Main, usa PanelPrincipal que sera una nueva clase.

--- src/main/java/grafica/Ventana.java
+++ src/main/java/grafica/Ventana.java
@@ -1,4 +1,18 @@
 package grafica;
 
-public class Ventana {
+import javax.swing.JFrame;
+
+public class Ventana extends JFrame {
+    public Ventana() {
+        setTitle("Aplicacion Expendedor");
+        setSize(1200, 600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+        setLocationRelativeTo(null);
+
+        PanelPrincipal panel = new PanelPrincipal();
+        setContentPane(panel);
+
+        setVisible(true);
+    }
 }
