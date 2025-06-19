81940cd6172f37d518cb5fc39dfaa783a2c9d98a
SebastianNovaOne <sebaxnovax@gmail.com>
2025-06-19 02:14:31 -0400

    Se añade constructor de PanelExpendedor, se añaden instanciaciones y metodos setBounds para probar tamaños despues. Se añade MouseListener para detectar clicks.

--- src/main/java/grafica/PanelExpendedor.java
+++ src/main/java/grafica/PanelExpendedor.java
@@ -2,6 +2,8 @@ package grafica;
 
 import javax.swing.*;
 import logica.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class PanelExpendedor extends JPanel {
 
@@ -10,4 +12,33 @@ public class PanelExpendedor extends JPanel {
     private PanelDepositoMoneda panelMonedasVuelto;
     private PanelDepositoMoneda panelMonedasIngresadas;
 
+    public PanelExpendedor(Expendedor expendedor) {
+        this.expendedor = expendedor;
+        this.setLayout(null);
+
+        int numProductos = 5;
+        panelesProductos = new PanelDepositoProducto[numProductos];
+
+        for (int i = 0; i < numProductos; i++) {
+            panelesProductos[i] = new PanelDepositoProducto(expendedor.getDepositoProducto(i));
+            panelesProductos[i].setBounds();
+            this.add(panelesProductos[i]);
+        }
+
+        panelMonedasVuelto = new PanelDepositoMoneda(expendedor.getDepositoVuelto());
+        panelMonedasVuelto.setBounds();
+        this.add(panelMonedasVuelto);
+
+        panelMonedasIngresadas = new PanelDepositoMoneda(expendedor.getDepositoMonedasIngresadas());
+        panelMonedasIngresadas.setBounds();
+        this.add(panelMonedasIngresadas);
+
+        this.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                rellenarDepositosVacios();
+                repaint();
+            }
+        });
+    }
 }
