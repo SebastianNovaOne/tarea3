45201f46a0a0c0f3f46b0b4678022dc986ac35b3
SebastianNovaOne <sebaxnovax@gmail.com>
2025-06-19 05:25:18 -0400

    Se añade paintComponent y zonas para elegir producto en PanelComprador.

--- src/main/java/grafica/PanelComprador.java
+++ src/main/java/grafica/PanelComprador.java
@@ -16,7 +16,33 @@ public class PanelComprador extends JPanel {
     public PanelComprador(PanelExpendedor exp) {
         this.expendedor = exp;
         this.zonas = new HashMap<>();
-        setBackground(Color.WHITE);
+        setBackground(Color.CYAN);
+
+        zonas.put("Moneda100", new Zona(30, 20, 100, 40, Color.ORANGE, "$100"));
+        zonas.put("Moneda500", new Zona(150, 20, 100, 40, Color.YELLOW, "$500"));
+        zonas.put("Moneda1000", new Zona(270, 20, 100, 40, new Color(0,100,0), "$1000"));
+
+        zonas.put("Producto1", new Zona(30, 100, 100, 40, Color.RED, "CocaCola"));
+        zonas.put("Producto2", new Zona(150, 100, 100, 40, Color.GREEN, "Sprite"));
+        zonas.put("Producto3", new Zona(270, 100, 100, 40, new Color(250, 112, 0), "Fanta"));
+        zonas.put("Producto4", new Zona(30, 160, 100, 40, new Color(150,75,0), "Snickers"));
+        zonas.put("Producto5", new Zona(150, 160, 100, 40, Color.LIGHT_GRAY, "Super8"));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        g.setColor(Color.BLACK);
+        g.setFont(new Font("Arial", Font.BOLD, 14));
+
+        g.drawString("1) Elección de moneda", 30, 15);
+
+        g.drawString("2) Elección de producto", 30, 85);
+
+        for (Zona z : zonas.values()) {
+            z.dibujar(g);
+        }
     }
 
     private static class Zona {
