1f77caa0105a7b58a0e4d2f1428d4af376fd2fbc
SebastianNovaOne <sebaxnovax@gmail.com>
2025-06-20 00:41:42 -0400

    Se añade documentacion Javadoc de Main.

--- src/main/java/grafica/Main.java
+++ src/main/java/grafica/Main.java
@@ -1,6 +1,17 @@
 package grafica;
 
+/**
+ * Ejecuta la aplicacion creando la ventana principal donde el usuario podra interactuar con el expendedor.
+ *
+ * Clase con metodo main que inicia la simulacion del expendedor de dulces y bebidas.
+ */
 public class Main {
+
+    /**
+     * Metodo principal que inicia al expendedor.
+     *
+     * @param args del metodo main
+     */
     public static void main(String[] args) {
         new Ventana();
     }
