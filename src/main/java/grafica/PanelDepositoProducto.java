86becd4610820f878cc377d514d769a0cc973c04
SebastianNovaOne <sebaxnovax@gmail.com>
2025-06-20 00:18:52 -0400

    Se añade documentacion Javadoc de PanelDepositoProducto.

--- src/main/java/grafica/PanelDepositoProducto.java
+++ src/main/java/grafica/PanelDepositoProducto.java
@@ -4,15 +4,28 @@ import javax.swing.*;
 import java.awt.*;
 import logica.*;
 
+/**
+ * Panel que representa visualmente un deposito de productos dentro del expendedor.
+ */
 public class PanelDepositoProducto extends JPanel {
 
     private Deposito<Producto> deposito;
 
+    /**
+     * Constructor que recibe el deposito de productos a mostrar.
+     *
+     * @param deposito deposito de productos a mostrar en el panel
+     */
     public PanelDepositoProducto(Deposito<Producto> deposito) {
         this.deposito = deposito;
         this.setPreferredSize(new Dimension(80,150));
     }
 
+    /**
+     * Dibuja el contenido del deposito con colores segun el producto.
+     *
+     * @param g objeto Graphics para pintar
+     */
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -21,7 +34,7 @@ public class PanelDepositoProducto extends JPanel {
         g.fillRect(0, 0, getWidth(), getHeight());
 
         g.setColor(Color.BLACK);
-        g.drawRect(0, 0, getWidth()-1, getHeight()-1);
+        g.drawRect(0,0,getWidth() - 1,getHeight() - 1);
 
         int y = 10;
         for (int i = 0; i < deposito.getCantidad(); i++) {
